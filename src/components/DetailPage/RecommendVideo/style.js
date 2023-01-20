d534072271f04ba0730b4d90369b21b0704f0a93
Minwoozzang <kminu0819@naver.com>
2023-01-20 19:51:27 +0900

    feat. detail ui 일부 작업

--- src/components/DetailPage/RecommendVideo/style.js
+++ src/components/DetailPage/RecommendVideo/style.js
@@ -1,5 +1,45 @@
 import styled from 'styled-components';
 
 export const RecommendBody = styled.div`
-  background-color: red;
+  width: 100%;
+  height: 100%;
+`;
+
+export const ViewBody = styled.div`
+  width: 80%;
+  height: 18%;
+
+  padding: 1%;
+  margin-left: 11%;
+  margin-top: 3%;
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const VideoView = styled.img`
+  width: 170px;
+  height: 130px;
+  border-radius: 10px;
+
+  margin-right: 3%;
+`;
+
+export const TextBody = styled.div`
+  width: 60%;
+  height: 86%;
+`;
+
+export const VideoText = styled.div`
+  font-weight: 600;
+  margin-bottom: 8%;
+`;
+
+export const VideoNickName = styled.div`
+  font-size: 0.9rem;
+  margin-bottom: 2%;
+`;
+
+export const VideoTime = styled.div`
+  font-size: 0.9rem;
 `;
