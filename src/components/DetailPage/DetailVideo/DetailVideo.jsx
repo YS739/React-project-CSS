a6bd9f3b3b241d6c3d1a94aed1d378a6e984f735
heerachoi <heera7722@gmail.com>
2023-01-30 11:31:29 +0900

    Style: 회원가입 성공문구, 반응형 수정

--- src/components/DetailPage/DetailVideo/DetailVideo.jsx
+++ src/components/DetailPage/DetailVideo/DetailVideo.jsx
@@ -28,7 +28,7 @@ const DetailVideo = () => {
             id="player"
             type="text/html"
             width="100%"
-            height="700px"
+            height="600px"
             src={`https://www.youtube.com/embed/${video.id.videoId}`}
             frameBorder="0"
             title="유튜브"
--- src/components/DetailPage/RecommendVideo/style.ts
+++ src/components/DetailPage/RecommendVideo/style.ts
@@ -2,38 +2,31 @@ import styled from 'styled-components';
 
 export const RecommendBody = styled.div`
   width: 100%;
-  height: 18vh;
-  margin-left: 11%;
+  margin-left: 3%;
   padding: 1%;
 
   cursor: pointer;
-  @media screen and (max-width: 1200px) {
-    width: 100%;
+  @media screen and (max-width: 1050px) {
+    width: 160%;
+  }
+    @media screen and (max-width: 700px) {
+    width: 90%;
   }
 `;
 
 export const ViewBody = styled.div`
-  width: 80%;
-  height: 100%;
-
   display: flex;
-
   margin-bottom: 2%;
 `;
 
 export const VideoViewBox = styled.div`
-  width: 40%;
-  height: 100%;
-  @media screen and (max-width: 1200px) {
-    width: 100%;
-  }
+  width: 180px;
 `;
 
 export const VideoView = styled.img`
   width: 100%;
   height: 100%;
   border-radius: 10px;
-
   margin-right: 3%;
   @media screen and (max-width: 1200px) {
     width: 170px;
@@ -43,7 +36,6 @@ export const VideoView = styled.img`
 export const VideoViewTextBody = styled.div`
   width: 58%;
   margin-left: 3%;
-
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -54,15 +46,10 @@ export const VideoViewTextBody = styled.div`
 
 export const VideoTextBody = styled.div`
   width: 100%;
-  /* height: 60%; */
 `;
 
 export const VideoText = styled.div`
-  /* width: 90%;
-  height: 100%; */
-  /* margin-top: 2%; */
-
-  font-size: 1.1rem;
+  font-size: 1rem;
   font-weight: 600;
 `;
 
@@ -72,12 +59,12 @@ export const VideoTitle = styled.div`
 `;
 
 export const VideoNickName = styled.div`
-  font-size: 0.95rem;
+  font-size: 0.8rem;
   margin-bottom: 2%;
   color: rgb(150, 150, 150);
 `;
 
 export const VideoTime = styled.div`
-  font-size: 0.95rem;
+  font-size: 0.8rem;
   color: rgb(150, 150, 150);
 `;
